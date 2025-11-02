50db9075b8e1f5a03ee480caad6da52e1efc0da9
Giulio Seregni <giulio.seregni@pingendo.com>
2025-11-02 13:47:28 +0100

    m

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    server: {
+      port: 3000,
+      allowedHosts:true
+    },
+    preview: {
+      
+      allowedHosts:true
+    },
     plugins: [tailwindcss()],
   }
 });
